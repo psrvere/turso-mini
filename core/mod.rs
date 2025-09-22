09eb2027db253f44397c548019f160be4538eae4
Prateek Singh Rathore <prateek.singh.rathore@gmail.com>
2025-09-22 15:59:03 +0530

    Added Completion and its implementation
    Signed-off-by: Prateek Singh Rathore <prateek.singh.rathore@gmail.com>

--- core/mod.rs
+++ core/mod.rs
@@ -1,9 +1,11 @@
 use bitflags::bitflags;
 use clock::Clock;
 use error::TursoMiniError;
-use std::sync::Arc;
+use core::fmt;
+use std::sync::{Arc, OnceLock};
 use buffer::Buffer;
 use error::CompletionError;
+use std::fmt::Debug;
 
 pub mod buffer;
 pub mod error;
@@ -96,6 +98,87 @@ pub enum CompletionType {
     Sync(SyncCompletion),
     Truncate(TruncateCompletion),
 }
+
+impl Debug for CompletionType {
+     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Read(..) => f.debug_tuple("Read").finish(),
+            Self::Write(..) => f.debug_tuple("Write").finish(),
+            Self::Sync(..) => f.debug_tuple("Sync").finish(),
+            Self::Truncate(..) => f.debug_tuple("Truncate").finish(),
+        }
+     }
+}
+
+#[derive(Debug)]
+struct CompletionInner {
+    completion_type: CompletionType,
+    result: OnceLock<Option<CompletionError>>,
+}
+
+pub struct Completion {
+    inner: Arc<CompletionInner>,
+}
+
+impl Completion {
+    pub fn new(completion_type: CompletionType) -> Self{
+        Self{
+            inner: Arc::new(CompletionInner { 
+                completion_type: completion_type, 
+                result: OnceLock::new(), 
+            }),
+        }
+    }
+
+    // Q. Why do we use Generic Type F and not WriteComplete directly?
+    // WriteComplete is a trait object (unsized type)
+    // Function parameters must be sized
+    // So we will have to pass it as Box<WriteComplete> which makes
+    // it inconvenient for oter devs to use this function as they have
+    // to mnaully box it everytime
+
+    // Q. Why use static lifetime here?
+    // It's a best practice to use static lifetime with callback function signatures
+    // Callbacks are called asynchronously, hence they must not have any dangling
+    // references. Also, with this callbacks can be safely moved between threads
+    pub fn new_write<F>(complete: F) -> Self
+    where
+        F: Fn(Result<i32, CompletionError>) + 'static,
+    {
+        Self::new(CompletionType::Write(WriteCompletion::new(
+            Box::new(complete)
+        )))
+    }
+
+    pub fn new_read<F>(buf: Arc<Buffer>, complete: F) -> Self
+    where
+        F: Fn(Result<(Arc<Buffer>, i32), CompletionError>) + 'static,
+    {
+            Self::new(CompletionType::Read(ReadCompletion::new(
+                buf, 
+                Box::new(complete),
+            )))
+    }
+
+    pub fn new_sync<F>(complete: F) -> Self
+    where 
+        F: Fn(Result<i32, CompletionError>) + 'static
+    {
+        Self::new(CompletionType::Sync(SyncCompletion::new(
+            Box::new(complete),
+        )))
+    }
+
+    pub fn new_trunc<F>(complete: F) -> Self
+    where 
+        F: Fn(Result<i32, CompletionError>) + 'static
+    {
+        Self::new(CompletionType::Truncate(TruncateCompletion::new(
+            Box::new(complete),
+        )))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::OpenFlags;
